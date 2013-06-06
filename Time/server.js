478f1ce4330bdb1c1906283b71a4c6a2bbb6aaf8
germanramos <german.ramos@gmail.com>
2013-06-06 14:17:11 +0200

    Time service and client

---
+++ Time/server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+
+if (process.argv.length != 5) {
+	console.log("Usage: node " + process.argv[1] + " listen_host port delay_ms");
+	console.log("Example: node " + process.argv[1] + " 0.0.0.0 1337 3000");
+	process.exit();
+}
+
+var host = process.argv[2]; //'0.0.0.0'
+var port = parseInt(process.argv[3],10); //1337
+var delay = parseInt(process.argv[4],10); //3000
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-Type' : 'text/plain',
+		'Access-Control-Allow-Origin': '*'
+	});
+	setTimeout(function() {
+		var now = new Date();
+		res.end(now.getUTCHours() + ':' + now.getUTCMinutes() + ':' + now.getUTCSeconds() + '\n');
+	}, delay);
+}).listen(port, host);
+console.log('Server running at http://' + host + ':' + port);
