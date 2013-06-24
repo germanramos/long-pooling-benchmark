daf2675c31ed88a49ab01049395f1ed04ef667a6
apenedo <icedfiend@gmail.com>
2013-06-24 16:00:39 +0200

    modified clientNode to expose REST api for demo porpuses

--- Time/clientNode.js
+++ Time/clientNode.js
@@ -1,38 +1,95 @@
 require('http').globalAgent.maxSockets = 100000;
-var hydra = require("../../hydra/src/hydra-node");
-var request = require("request");
+var hydra = require('../../hydra/src/hydra-node');
+	request = require('request'),
+	express = require('express'),
+	app = express();
 
-var service = "time";
-var hydraRefreshWait = 1000;
-var errorWait = 2000;
-var ajaxTimeout = 4000;
-var randomWait = 5000;
-var blacklistTime = 5000;
+var port = parseInt(process.argv[2],10) || 5000;
 
-var servers = [];
-hydra.config([ "http://hydra.cloud1.com:7001" ]);
-var blacklist = [];
+var service = "time",
+	hydraRefreshWait = 1000,
+	errorWait = 2000,
+	ajaxTimeout = 4000,
+	randomWait = 5000,
+	blacklistTime = 5000,
+	updateTimeout = null,
+	intervals = [],
+	servers = [],
+	blacklist = [],
+	pendingRequests = [],
+	stop = false;
 
-updateServers();
+app.configure(function() {
+	app.use( express.static(__dirname + '/www') );
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
+});
+
+app.get('/start/:connections', function(req, res){
+	var hydraServer = req.query.hydra || 'http://1.hydra.innotechapp.com:80';
+	stopRequests();
+	startRequests((parseInt(req.params.connections,10) || 1), hydraServer);
+	res.send(200, {count: intervals.length, requests: pendingRequests.length});
+});
+
+app.get('/info', function(req, res){
+	res.send(200, {count: intervals.length, requests: pendingRequests.length});
+});
+
+app.get('/stop', function(req, res){
+	stopRequests();
+	res.send(200, {count: intervals.length, requests: pendingRequests.length});
+});
+
+app.listen(port);
+console.log('Stress-time listening on port', port);
+
+function startRequests(connections, hydraServer) {
+	console.log('Starting', connections, 'connections on', hydraServer);
+	hydra.config([ hydraServer ]);
+	intervals.push(setInterval(updateServers, hydraRefreshWait));
+
+	for ( var i = 0; i < connections; i++) {
+		intervals.push(setInterval(makeRequest, Math.floor( Math.random() * randomWait )));
+	}
+}
+
+function stopRequests() {
+	console.log('Stopping connections');
+	for (var r in pendingRequests) {
+		pendingRequests[r].abort();
+	}
+	pendingRequests = [];
+
+	for (var t in intervals) {
+		clearInterval(intervals[t]);
+	}
+	intervals = [];
+}
 
 function blacklistAdd(url) {
 	for (var i=0; i<blacklist.length; i++) {
 		if (blacklist[i].url == url) {
-			blacklist[i].timestamp = new Date().getTime();
+			blacklist[i].timestamp = Date.now();
 			return;
 		}
 	}
-	console.log("Adding " + url + " to blacklist")
-	blacklist.push({ url: url, timestamp : new Date().getTime()});
+	console.log("Adding " + url + " to blacklist");
+	blacklist.push({ url: url, timestamp : Date.now()});
 }
 
 function updateServers() {
 	//Get servers from hydra
-	hydra.get("time", true, function(err, result) {
-		if (result != null)
+	hydra.get(service, true, function(err, result) {
+		if (result !== null){
 			//Clean blacklist and filter servers
-			var newblacklist = []
-			var now = new Date().getTime();
+			var newblacklist = [];
+			var now = Date.now();
 			for (var i=0; i<blacklist.length; i++) {
 				if (now - blacklist[i].timestamp < blacklistTime) {
 					newblacklist.push(blacklist[i]);
@@ -40,50 +97,36 @@ function updateServers() {
 					console.log("Removing " + blacklist[i].url + " from blacklist");
 					continue;
 				}
-				var index = result.indexOf(blacklist[i].url) 
+				var index = result.indexOf(blacklist[i].url);
 				if (index >= 0) {
 					console.log("Removing " + blacklist[i].url + " from updated server list");
 					result.splice(index, 1);
 				}
 			}
 			blacklist = newblacklist;
-			servers = result;	
-	})
-
-	//Program refresh
-	setTimeout(updateServers, hydraRefreshWait);
+			servers = result;
+		}
+	});
 }
 
 function makeRequest() {
-	if (servers == null || servers.length < 1) {
-		setTimeout(makeRequest, errorWait);
+	if (servers === null || servers.length < 1) {
 		return;
 	}
 	var url = servers[0];
 	var options = {
 		url : url,
-		timeout : 6000,
-		//agent: false
-	}
-	request(options, function(error, response, body) {
-		//console.log(body);
+		timeout : 6000
+	};
+	var r = request(options, function(error, response, body) {
+		pendingRequests = pendingRequests.splice(pendingRequests.indexOf(r),1);
+
+		var customWait = 0;
 		if (response && response.statusCode && response.statusCode == 200) {
-			var customWait = randomWait;
-			//console.log("Ok");
+			customWait = randomWait;
 		} else {
-			//console.log("Error");
-			//servers.push(servers.shift());	
-			//servers.splice(servers.indexOf(url), 1);
-			var customWait = 0;
 			blacklistAdd(url);
 		}
-		setTimeout(makeRequest, Math.floor(Math.random()*customWait));
-		//makeRequest();
 	});
-
-}
-
-for ( var i = 0; i < process.argv[2]; i++) {
-	setTimeout(makeRequest, Math.floor(Math.random()*randomWait));
-	//makeRequest();
+	pendingRequests.push(r);
 }
